70bac252669839b8614f2941cd567bbf6fdcdf0e
swapnilsupe01 <144266707+swapnilsupe01@users.noreply.github.com>
2023-09-29 16:35:02 +0000

    Datatype

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert() we are usinh nodejs ,nort browser
+
+//console.log(13+3);   // code readability be high
+//console.log("Swapnil");
+
+let name = "Swapnil";
+let age = 20;
+let isLoggedIn = false;
+let state;   //variable declare but value is not assign means it is undefined
+
+//Primative datatype
+// number => 2 t power 53
+// bigint =>
+//String => ""
+// boolean => true/false
+// null => standalone value   // it is representation of empty value means ka kali hai example is wather app give respone that i dont get any value of temperature the is send me null insted of zero bez zero mit be temperature  so that why  
+// undefined =>
+//symbol it is used in react (for uniqueness)
+
+// Type of variable is used in js
+//console.log(typeof "Swapnil"); //give string
+//console.log(typeof 11);  //give return number
+console.log(typeof null);    // that gives the object 
+console.log(typeof undefined); //output is undefined
