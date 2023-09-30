e9f9b5c7d2f9ae4709a7a0fe18384578770e9832
swapnilsupe01 <144266707+swapnilsupe01@users.noreply.github.com>
2023-09-30 18:36:08 +0000

    New Conversion

---
+++ 01_basic/03_converOperation.js
@@ -0,0 +1,68 @@
+//let score = 28;
+//let score11="33asd";  //string because it is in "" quote
+//let score11=nul
+//let score11 = true;
+
+//Two type of typeof 
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//console.log(score11);
+// console.log(typeof score11);    // it is display typeof of score11  
+// let value = Number(score11);  //conversion of score11 into the Number
+// console.log(typeof value);
+// console.log(value); //it is display an NaN(means Not a Number) fter the conversion
+
+
+//let valueInNumber = Number(score);    //conversion of score into he Number 
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*
+null is typeof is object 
+but after conversion to Number that null turn into zero 
+*/
+
+/*
+true is typeof is boolean 
+but after conversion to Number that null turn into 1 
+true =>1, false=>0
+*/
+
+// let isLogged ="";   // 1=>true ; 0=>false
+// let booleanIsLogged = Boolean(isLogged);  //conversion into boolean 
+ 
+//console.log(booleanIsLogged);
+/*
+"swap"=> give true value
+*/
+/*
+""=>  give false value                 
+*/
+
+// let someNum = 22;
+// let StringNum = String(someNum);
+// console.log(StringNum);    //display number 22
+// console.log(typeof StringNum);   //but Number us convert into string  
+
+// ***************************** Opertions *************************
+
+//let value=3;
+//let negValue = -value;  //that give negative value
+//console.log(negValue);  
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/3);
+// console.log(2%3);
+
+//console.log("1" + 2); //it take first sting the after that it take all number as string itself
+//console.log(1 + 2 + "2"); // it perform first operation after that last one is treat as string
+
+console.log(+true); //gives the 1 beacuase true is boolean after conversion it is increement 
+//console.log(true+);  //Not accept
+console.log(+"");  //give zero because boolean 
+
+// link to study 
+//https://tc39.es/ecma262/multipage/abstract-operation.html#sec-type-conversion 
